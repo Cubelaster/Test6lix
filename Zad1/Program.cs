9487d1e2da4a4c50df4d0742049fa7ce19d0629f
Cubelaster <david.cubela@gmail.com>
2017-12-05 22:44:13 +0100

    Finished with first 3

---
+++ Zad1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zad1
+{
+    class Program
+    {
+        public static int MaxDepth { get; set; }
+        public static int CurrentDepth { get; set; }
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> TreeDict = new Dictionary<int, string>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                TreeDict.Add(i, args[i]);
+            }
+            var TreeValues = TreeDict.Select(x => int.Parse(x.Value));
+            var LeafNodes = TreeDict.Keys.Where(key => !(TreeValues.Contains(key)));
+            GetMaxDepth(TreeDict);
+            Console.WriteLine("Max Tree depth is: " + MaxDepth);
+            Console.WriteLine("Number of leaves in the tree is :" + LeafNodes.Count());
+        }
+
+        public static void GetMaxDepth(Dictionary<int, string> TreeDict)
+        {
+            foreach (var i in TreeDict.Keys)
+            {
+                GetParent(i, TreeDict);
+            }
+        }
+
+        public static void GetParent(int child, Dictionary<int, string> TreeDict)
+        {
+            TreeDict.TryGetValue(child, out string parent);
+            if (int.Parse(parent) > -1)
+            {
+                CurrentDepth++;
+                GetParent(int.Parse(parent), TreeDict);
+            }
+            else
+            {
+                MaxDepth = MaxDepth < CurrentDepth ? CurrentDepth : MaxDepth;
+                CurrentDepth = 0;
+            }
+        }
+
+    }
+}
---
+++ Zad3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            foreach (string arg in args)
+            {
+                path += " " + arg;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            XDocument xDocument = XDocument.Load(path);
+
+            var root = xDocument.Elements("sheet").ToList();
+            var rows = root.Elements("row");
+
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                Console.WriteLine("Insert coordinates");
+                var coordinates = Console.ReadLine();
+                int[] coordinateArray = coordinates.Split(' ').Select(x => int.Parse(x)).ToArray();
+                if (coordinateArray[0] == -1 && coordinateArray[1] == -1)
+                {
+                    break;
+                }
+                GetCells(coordinateArray, rows);
+            }
+        }
+
+        static void GetCells(int[] coordinates, IEnumerable<XElement> Rows)
+        {
+            if (Rows.ToList().Count <= coordinates[0])
+            {
+                Console.WriteLine("Out of bounds, select new coordinates - Empty");
+                return;
+            }
+            var Cells = Rows.ElementAt(coordinates[0]).Elements("cell");
+            if (Cells.ToList().Count <= coordinates[1])
+            {
+                Console.WriteLine("Out of bounds, select new coordinates - Empty");
+                return;
+            }
+            var cellValue = Cells.ElementAt(coordinates[1]).Value;
+            Console.WriteLine(cellValue);
+        }
+    }
+}
